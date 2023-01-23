e485e6af06f6f725c709ea888d175746dbf7ae2e
magraina <marinafcegner@gmail.com>
2023-01-23 20:50:55 +0100

    feat(Session): Added function for logout and to check if session is valid

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use-strict';
 
+import {checkSession} from './modules/checkSession.js';
 import {createSession} from './modules/createSession.js';
+import {destroySession} from './modules/destroySession.js';
 
 /**
  * Login request to Fritz!OS with use of PBKDF2 (Requires Fritz!OS 7.24) or MD5 (Requires Fritz!OS 5.50) Challenge-Response Process.
@@ -14,12 +16,45 @@ import {createSession} from './modules/createSession.js';
  * @returns {Promise<Object>} Response session data as Object
  */
 export const doInitSession = function({host, user, password, mode, useSSL}) {
-	return createSession.getSession({
-		host,
+	return createSession.getSession({host,
 		mode,
 		password,
 		useSSL,
-		user,
-	});
+		user});
 };
 
+/**
+ * Check if Session is valid
+ * @async
+ * @function
+ * @param {Object} connection - connection details
+ * @param {String} connection.host - hostname or IP-Address
+ * @param {Number} connection.sessionId - current session ID
+ * @param {Number} [connection.mode='PBKDF2'] - Challenge-Response Process; either 'PBKDF2' (default) or 'MD5'
+ * @param {Boolean} [connection.useSSL=false] - true if SSL connection over https should be used (default is false)
+ * @return {Promise<Boolean>} Response if Session is valid
+ */
+export const isValidSession = function({host, sessionId, mode, useSSL}) {
+	return checkSession.isValidSession({host,
+		mode,
+		sessionId,
+		useSSL});
+};
+
+/**
+ * Logout user from the Fritz!OS interface
+ * @async
+ * @function
+ * @param {Object} connection - connection details
+ * @param {String} connection.host - hostname or IP-Address
+ * @param {Number} connection.sessionId - current session ID
+ * @param {Number} [connection.mode='PBKDF2'] - Challenge-Response Process; either 'PBKDF2' (default) or 'MD5'
+ * @param {Boolean} [connection.useSSL=false] - true if SSL connection over https should be used (default is false)
+ * @return {Promise<Object>} Response session data as Object
+ */
+export const doEndSession = function({host, sessionId, mode, useSSL}) {
+	return destroySession({host,
+		mode,
+		sessionId,
+		useSSL});
+};
---
+++ modules/destroySession.js
@@ -0,0 +1,51 @@
+'use-strict';
+
+/**
+ * @module destroySession
+ * @author Marina Egner <marinafcegner@sheepCreativeStudios.de>
+ * @copyright Marina Egner 2023
+ */
+import {request} from './request.js';
+import {XMLParser} from 'fast-xml-parser';
+
+// Define some Magic numbers for the version which is provided to the login service
+const version = {
+	MD5: 1,
+	PBKDF2: 2,
+};
+
+/**
+ * Logout user from the Fritz!OS interface
+ * @async
+ * @function
+ * @param {Object} connection - connection details
+ * @param {String} connection.host - hostname or IP-Address
+ * @param {Number} connection.sessionId - current session ID
+ * @param {Number} [connection.mode='PBKDF2'] - Challenge-Response Process; either 'PBKDF2' (default) or 'MD5'
+ * @param {Boolean} [connection.useSSL=false] - true if SSL connection over https should be used (default is false)
+ * @return {Promise<Object>} Response session data as Object
+ */
+const destroySession = async function({host, sessionId, mode='PBKDF2', useSSL=false}) {
+	let processVersion = version.MD5;
+
+	// Force mode if selected
+	if(mode === 'PBKDF2') processVersion = version.PBKDF2;
+	const response = await request.httpGetRequest({
+		host,
+		parameters: new URLSearchParams({
+			logout: sessionId,
+			sid: sessionId,
+			version: processVersion,
+		}),
+		service: 'login_sid.lua',
+		useSSL,
+	}).
+		catch((error) => {
+			// If request fails, reject with error message
+			return Promise.reject(error);
+		});
+	const parser = new XMLParser({ignoreDeclaration: true});
+	return Promise.resolve(parser.parse(response));
+};
+
+export {destroySession};
